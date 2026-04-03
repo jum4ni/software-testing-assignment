1cf1b26e9c25b6d33f1c5fcffdb03c2fc31fba43
jum4ni <nieminenj@gmail.com>
2026-04-03 15:41:28 +0300

    Update upperFirst.test.js

--- upperFirst.test.js
+++ upperFirst.test.js
@@ -1,9 +1,11 @@
 import upperFirst from './src/upperFirst.js'
 
+// Testataan että ensimmäinen  merkki muutetaan isoksi
 test('uppercases first character', () => {
   expect(upperFirst('hello')).toBe('Hello')
 })
 
+// Testataan tyhjä merkkijono (reunatapaus)
 test('handles empty string', () => {
   expect(upperFirst('')).toBe('')
 })
