a62e4ced1b2b32985238962f18137a73640e5fba
jum4ni <nieminenj@gmail.com>
2026-04-03 15:26:58 +0300

    Update slice.test.js

--- slice.test.js
+++ slice.test.js
@@ -1,21 +1,26 @@
 import slice from './src/slice.js'
 
+// Testataan merkkijonon pilkkominen aloitus- ja lopetuksien välillä
 test('slices string with start and end', () => {
   expect(slice('abcdef', 1, 4)).toEqual(['b', 'c', 'd'])
 })
 
+// Testataan pilkkominen aloitusindeksistä loppuun asti
 test('slices string from start index to end', () => {
   expect(slice('abcdef', 2)).toEqual(['c', 'd', 'e', 'f'])
 })
 
+// Testataan negatiivinen alotusindeksi (lopusta laskeminen)
 test('slices string with negative start', () => {
   expect(slice('abcdef', -2)).toEqual(['e', 'f'])
 })
 
+// Testataan taulukon pilkkominen
 test('slices array with start and end', () => {
   expect(slice([1, 2, 3, 4], 1, 3)).toEqual([2, 3])
 })
 
+// Testataan tapaus jossa alku ja loppu ovat samat (palauttaa tyhjän)
 test('returns empty array when start equals end', () => {
   expect(slice('abc', 1, 1)).toEqual([])
 })
