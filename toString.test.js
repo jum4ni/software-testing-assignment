e7a32b0c567bf7f60f73ff621c0a430ede2f67fa
jum4ni <nieminenj@gmail.com>
2026-04-03 15:38:05 +0300

    Update toString.test.js

--- toString.test.js
+++ toString.test.js
@@ -1,21 +1,26 @@
 import toString from './src/toString.js'
 
+// Testaan numeron muuntaminen merkkijonoksi
 test('converts number to string', () => {
   expect(toString(5)).toBe('5')
 })
 
+// Testaan että merkkijono pysyy samana
 test('converts string to string', () => {
   expect(toString('abc')).toBe('abc')
 })
 
+// Testataan endefined- arvon käsittely (reunatapaus)
 test('handles undefined value', () => {
   expect(toString(undefined)).toBe('undefined')
 })
 
+// Testataan taulukon muuntaminen merkkijonoksi
 test('converts array to string', () => {
   expect(toString([1, 2, 3])).toBe('1,2,3')
 })
 
+// Testataan negatiivisen nollan käsittely
 test('handles negative zero', () => {
   expect(toString(-0)).toBe('-0')
 })
